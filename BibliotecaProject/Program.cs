dad66e720bbd1ad6453448a7fb6037bdd3f9cd84
Reinan <reinanlanz@gmail.com>
2024-09-23 14:56:38 -0300

    Freature: program

--- BibliotecaProject/Program.cs
+++ BibliotecaProject/Program.cs
@@ -25,11 +25,9 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+
 
 app.UseHttpsRedirection();
 
